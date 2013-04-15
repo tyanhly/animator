b9444d6b065a12ac45631a28fe05a9fc7235e805
tyanhly <tyanhly@tyanhly-System-Product-Name>
2013-04-15 09:29:53 +0700

    init

---
+++ animator/test/com/example/animator/MainActivityTest.java
@@ -0,0 +1,13 @@
+package com.example.animator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+//@RunWith(org.robolectric.RobolectricTestRunner.class)
+public class MainActivityTest {
+
+    @Test
+    public void testA(){
+        
+    }
+}
