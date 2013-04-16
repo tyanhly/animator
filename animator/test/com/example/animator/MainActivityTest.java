2bb23050a560b1e33ee8e12be828be12ab8cba27
tyanhly <tyanhly@tyanhly-System-Product-Name>
2013-04-16 12:05:59 +0700

    complete

--- animator/test/com/example/animator/MainActivityTest.java
+++ animator/test/com/example/animator/MainActivityTest.java
@@ -3,11 +3,13 @@ package com.example.animator;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-//@RunWith(org.robolectric.RobolectricTestRunner.class)
+@RunWith(org.robolectric.RobolectricTestRunner.class)
 public class MainActivityTest {
-
+    
     @Test
-    public void testA(){
-        
+    public void testA() {
+        MainActivity mainActivity = new MainActivity();
+        mainActivity.onCreate(null);
+        // Assert.assertEquals(1, 1);
     }
 }
